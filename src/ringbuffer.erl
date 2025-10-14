ca920a03982ee995886727d15cf128767d848468
Marc Worrell <marc@worrell.nl>
2025-10-14 13:40:40 +0200

    Add ringbuffer:whereis/1, update doc styling and CI

--- src/ringbuffer.erl
+++ src/ringbuffer.erl
@@ -1,5 +1,5 @@
 %% @author Marc Worrell <marc@worrell.nl>
-%% @copyright 2021 Marc Worrell
+%% @copyright 2021-2025 Marc Worrell
 %% @doc Ringbuffer implements a length limited queue. In systems this is
 %% often implemented as a ring, or cylic, buffer. Where the writer can
 %% push the reader ahead if the buffer is full.
@@ -26,8 +26,9 @@
 %% <li>As many queues as needed.</li>
 %% </ul>
 %%
+%% @end
 
-%% Copyright 2021 Marc Worrell
+%% Copyright 2021-2025 Marc Worrell
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -46,6 +47,7 @@
 -export([
     start/0,
     new/2,
+    whereis/1,
     delete/1,
     write/2,
     read/1
@@ -63,21 +65,40 @@ start() ->
 %% @doc Create a new named buffer of Size entries. The name must be unique for all ets tables.
 %% The name must be an atom, and is used for the name of the ets table. A process owning the
 %% ets table and synchronizing the readers is added to <tt>ringbuffer_sup</tt>.
--spec new( Name :: atom(), Size :: pos_integer() ) -> {ok, pid()} | {error, term()}.
-new( Name, Size ) ->
-    ringbuffer_sup:start_child(Name, Size).
+-spec new( Name, Size ) -> {ok, pid()} | {error, Reason} when
+    Name :: atom(),
+    Size :: pos_integer(),
+    Reason :: {already_started, pid()} | term().
+new(Name, Size) ->
+    case ?MODULE:whereis(Name) of
+        undefined ->
+            ringbuffer_sup:start_child(Name, Size);
+        Pid ->
+            {error, {already_started, Pid}}
+    end.
 
+%% @doc Find the ringbuffer with a certain name, return the pid if found, otherwise undefined.
+-spec whereis(Name) -> pid() | undefined when
+    Name :: atom().
+whereis(Name) ->
+    case ringbuffer_process:process_pid(Name) of
+        {ok, Pid} -> Pid;
+        {error, _} -> undefined
+    end.
 
 %% @doc Delete a named ringbuffer, all queued data is destroyed. The ets table and the
 %% synchronizing process are deleted.
--spec delete( Name :: atom() ) -> ok | {error, not_found}.
+-spec delete(Name) -> ok | {error, not_found} when
+    Name :: atom().
 delete(Name) ->
     ringbuffer_sup:stop_child(Name).
 
 
 %% @doc Add an entry to the named ringbuffer. Never fails, if the ringbuffer
 %% is full then older entries are overwritten.
--spec write( Name :: atom(), Payload :: term() ) -> ok.
+-spec write(Name, Payload) -> ok when
+    Name :: atom(),
+    Payload :: term().
 write(Name, Payload) ->
     ringbuffer_process:write(Name, Payload).
 
@@ -85,7 +106,10 @@ write(Name, Payload) ->
 %% the payload of the entry read. An entry is skipped if the readers are falling behind the
 %% writers by more that the size of the buffer. <tt>{error, empty}</tt> is returned if the
 %% buffer is empty.
--spec read( Name :: atom() ) -> {ok, {non_neg_integer(), term()}} | {error, empty}.
+-spec read( Name ) -> {ok, {SkipCount, Payload}} | {error, empty} when
+    Name :: atom(),
+    SkipCount :: non_neg_integer(),
+    Payload :: term().
 read(Name) ->
     ringbuffer_process:read(Name).
 
